6f16742f28006f6d5e9daa8c2da45f4d3a41b3bc
asanahmadli <asenahmedli@gmail.com>
2024-04-25 22:15:44 -0400

    I added passion project into github account

---
+++ carmech/src/main/java/com/zipcode/carmech/CarmechApplication.java
@@ -0,0 +1,13 @@
+package com.zipcode.carmech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarmechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarmechApplication.class, args);
+	}
+
+}
